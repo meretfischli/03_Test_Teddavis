c881517f679df4c2e35d78898ea178e3e2a588b2
Meret Fischli <merfisch@bluewin.ch>
2022-11-28 12:03:45 +0100

    first_commit

---
+++ rect_random.js
@@ -0,0 +1,12 @@
+function draw(ctx) {
+    const canvas = document.getElementById('cnv1');
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+
+        canvas.width = 100
+        canvas.height = 100
+
+        ctx.strokeRect(pos, pos, size, size);
+        ctx.strokeRect(pos*2, pos*2, size/3, size/3);
+    }
+}
